f883d43cb02f0db28dca7decddab40b14284911c
Henrique Bissoli Silva <emp.shad@gmail.com>
2018-11-25 02:41:39 -0200

    Update shadowcap.sh

--- shadowcap.sh
+++ shadowcap.sh
@@ -2,6 +2,7 @@
 # ShadowCAP
 # @autor: Henrique Bissoli Silva (emp.shad@gmail.com)
 
+# CTRL + C
 exiting(){
 	tput clear
 	echo -e "${BOLD}Exiting..$NC"
@@ -14,18 +15,23 @@ if [ "$(id -u)" != "0" ]; then
 	exit 1	
 fi
 
-# User INFO
-GETPEER=$(ip -f inet a|grep -oP "(?<=inet ).+(?=\/)" | grep peer)
-
 # Check VPN
+GETPEER=$(ip -f inet a|grep -oP "(?<=inet ).+(?=\/)" | grep peer)
 if [[ "$GETPEER" != '' ]]; then
 	echo "Disable VPN to use this script" 1>&2
 	exit 1
 fi
 
+# User INFO
 IFACE=$(ip route |grep default |sed -e "s/^.*dev.//" -e "s/.proto.*//")
 IP=$(hostname -I |cut -d' ' -f1)
 MAC=$(ip a |awk '/ether/ {print $2}'|head -n 1)
 IPRANGE=$(ip a s|grep -A8 -m1 MULTICAST|grep -m1 inet|cut -d' ' -f6)
+
+# Enable IP forwarding
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+# Packets targeting port 80 is delivered to local app listening on 8080
+iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 8080
 
 echo $IPRANGE
